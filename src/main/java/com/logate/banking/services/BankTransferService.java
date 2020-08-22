8f03439c5abd9ccd8aa76e5bd971b7323ba438a0
Vuk Adzic <adzic820@gmail.com>
2020-08-22 13:49:59 +0200

    - review correction done.
    Signed-off-by: Vuk Adzic <adzic820@gmai.com>

--- src/main/java/com/logate/banking/services/BankTransferService.java
+++ src/main/java/com/logate/banking/services/BankTransferService.java
@@ -19,38 +19,58 @@ public class BankTransferService {
     @Autowired
     private UserService userService;
 
+    private BankAccount bankAccountFrom;
+    private BankAccount bankAccountTo;
+
     public boolean transfer(BankTransferDTO bankTransferDTO){
 
-        String username=SecurityContextHolder.getContext().getAuthentication().getName();
-        User fromUser = userService.findByUsername(username);
+        if(checkAccountsValidity(bankTransferDTO) && checkUserAuthority(bankTransferDTO)){
+            return doTransfer(bankTransferDTO,bankAccountFrom,bankAccountTo);
+        }
+        return false;
+    }
+
+
+    private boolean checkAccountsValidity(BankTransferDTO bankTransferDTO) {
+
         Optional<BankAccount> optBankAccFrom = bankAccountService.findByAccNumber(
-                                                                                bankTransferDTO.getFromAccountNumber());
+                bankTransferDTO.getFromAccountNumber());
         Optional<BankAccount> optBankAccTo= bankAccountService.findByAccNumber(
-                                                                                bankTransferDTO.getToAccountNumber());
+                bankTransferDTO.getToAccountNumber());
 
-        if(optBankAccFrom.isPresent() && optBankAccTo.isPresent()){
-            BankAccount bankAccountFrom = optBankAccFrom.get();
-            BankAccount bankAccountTo = optBankAccTo.get();
-
-            List<BankAccount> bankAccountList = fromUser.getBankAccounts();
-            List<String> bankAccNumbers = new ArrayList<>();
-            for (BankAccount bankAccount:bankAccountList) {
-                bankAccNumbers.add(bankAccount.getAccountNumber());
-            }
-
-            if (bankAccNumbers.contains(bankTransferDTO.getFromAccountNumber())
-                                        && bankAccountFrom.getCurrentBalance()>=bankTransferDTO.getAmount()
-                                        && bankTransferDTO.getAmount()>=0){
-                bankAccountFrom.setCurrentBalance(bankAccountFrom.getCurrentBalance()- bankTransferDTO.getAmount());
-                bankAccountTo.setCurrentBalance(bankAccountTo.getCurrentBalance() + bankTransferDTO.getAmount());
-                bankAccountService.update(bankAccountFrom);
-                bankAccountService.update(bankAccountTo);
+        if (optBankAccFrom.isPresent() && optBankAccTo.isPresent()) {
+            bankAccountFrom = optBankAccFrom.get();
+            bankAccountTo = optBankAccTo.get();
+            if (bankAccountFrom.getCurrentBalance() >= bankTransferDTO.getAmount()
+                    && bankTransferDTO.getAmount() >= 0) {
                 return true;
             }
         }
         return false;
-
     }
 
+    private boolean checkUserAuthority(BankTransferDTO bankTransferDTO){
+
+        String username=SecurityContextHolder.getContext().getAuthentication().getName();
+        User fromUser = userService.findByUsername(username);
+
+        List<BankAccount> bankAccountList = fromUser.getBankAccounts();
+        List<String> bankAccNumbers = new ArrayList<>();
+        for (BankAccount bankAccount:bankAccountList) {
+            bankAccNumbers.add(bankAccount.getAccountNumber());
+        }
+        return bankAccNumbers.contains(bankTransferDTO.getFromAccountNumber());
+    }
+
+    private boolean doTransfer(BankTransferDTO bankTransferDTO,
+                               BankAccount bankAccountFrom,
+                               BankAccount bankAccountTo){
+
+        bankAccountFrom.setCurrentBalance(bankAccountFrom.getCurrentBalance()- bankTransferDTO.getAmount());
+        bankAccountTo.setCurrentBalance(bankAccountTo.getCurrentBalance() + bankTransferDTO.getAmount());
+        bankAccountService.update(bankAccountFrom);
+        bankAccountService.update(bankAccountTo);
+        return true;
+    }
 
 }
